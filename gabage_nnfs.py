86ef7ff02f77370de96f296db6a7d43a17adfd7e
Mouhamed Moustapha KEITA <keitamoustapha913@gmail.com>
2020-07-28 15:47:03 +0200

    Using classes for batches of inputs and two layers

--- gabage_nnfs.py
+++ gabage_nnfs.py
@@ -47,3 +47,33 @@ output =  [ inputs[0]*weights1[0] + inputs[1]*weights1[1] + inputs[2]*weights1[2
             inputs[0]*weights3[0] + inputs[1]*weights3[1] + inputs[2]*weights3[2] + inputs[3]*weights3[3]+ bias3 ]
 
 '''
+
+
+'''
+# Having a batch of sensors or features
+inputs =  [ [1.0 , 2.0 , 3.0 , 2.5 ],
+            [2.0 , 5.0 , -1.0 , 2.0 ],
+            [-1.5, 2.7 , 3.3 , -0.8 ] ]
+
+weights = [ [0.2, 0.8, -0.5, 1.0],
+            [0.5, -0.91, 0.26, -0.5] ,
+            [-0.26, -0.27, 0.17, 0.87] ]
+
+biases = [ 2.0 , 3.0, 0.5 ]
+
+# adding an additional layer 
+weights2 = [ [0.1, -0.14 , 0.5 ],
+            [-0.5, 0.12 , -0.33 ] ,
+            [-0.44, 0.73 , -0.13] ]
+
+biases2 = [ -1, 2 , -0.5 ]
+
+
+# Using Dot product approach for two layer
+layer1_output = np.dot(inputs , np.array(weights).T ) + biases
+layer2_output = np.dot(layer1_output , np.array(weights2).T ) + biases2
+print( layer2_output )
+
+
+
+'''
--- nnfs.py
+++ nnfs.py
@@ -5,26 +5,30 @@ Associated YT tutorial: https://youtu.be/TEWy9vZcxW4
 '''
 import numpy as np 
 
-# Having a batch of sensors or features
-inputs =  [ [1.0 , 2.0 , 3.0 , 2.5 ],
+np.random.seed(0)
+
+# inputs
+X =  [ [1.0 , 2.0 , 3.0 , 2.5 ],
             [2.0 , 5.0 , -1.0 , 2.0 ],
             [-1.5, 2.7 , 3.3 , -0.8 ] ]
 
-weights = [ [0.2, 0.8, -0.5, 1.0],
-            [0.5, -0.91, 0.26, -0.5] ,
-            [-0.26, -0.27, 0.17, 0.87] ]
+class Layer_Dense:
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros( (1, n_neurons))
 
-biases = [ 2.0 , 3.0, 0.5 ]
+    def forward(self, inputs):
+        self.output = np.dot( inputs, self.weights) + self.biases
 
-# adding an additional layer 
-weights2 = [ [0.1, -0.14 , 0.5 ],
-            [-0.5, 0.12 , -0.33 ] ,
-            [-0.44, 0.73 , -0.13] ]
-
-biases2 = [ -1, 2 , -0.5 ]
+layer1 = Layer_Dense(4, 5 )
+layer2 = Layer_Dense(5 , 2 )
 
 
-# Using Dot product approach for two layer
-layer1_output = np.dot(inputs , np.array(weights).T ) + biases
-layer2_output = np.dot(layer1_output , np.array(weights2).T ) + biases2
-print( layer2_output )
+layer1.forward(X)
+print( layer1.output)
+
+layer2.forward( layer1.output)
+print( layer2.output)
+
+
+
