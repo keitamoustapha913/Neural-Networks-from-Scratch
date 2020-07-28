dc36b2c64c71bdff01107e412d6b608cfbf6cb76
Mouhamed Moustapha KEITA <keitamoustapha913@gmail.com>
2020-07-28 14:45:15 +0200

    Dot product for a layer of neurons

--- nnfs.py
+++ nnfs.py
@@ -1,12 +1,14 @@
 '''
 From Sentdex Youtube channel
-Creates 3 neurons, with 4 inputs using vector and matrices.
+Creates layers with batches of inputs or features or samples.
 Associated YT NNFS tutorial: https://www.youtube.com/watch?v=lGLto9Xd7bU
 '''
 import numpy as np 
 
-
-inputs = [1, 2 , 3, 2.5 ]
+# Having a batch of sensors or features
+inputs =  [ [1.0 , 2.0 , 3.0 , 2.5 ],
+            [2.0 , 5.0 , -1.0 , 2.0 ],
+            [-1.5, 2.7 , 3.3 , -0.8 ] ]
 
 weights = [ [0.2, 0.8, -0.5, 1.0],
             [0.5, -0.91, 0.26, -0.5] ,
@@ -16,6 +18,6 @@ biases = [ 2.0 , 3.0, 0.5 ]
 
 
 # Using Dot product approach for a layer
-output = np.dot(weights , inputs) + biases
 
+output = np.dot(inputs , np.array(weights).T ) + biases
 print( output )
