aa60705ea9f016b9a2acc4d31b02fa3deda95b5b
Mouhamed Moustapha KEITA <keitamoustapha913@gmail.com>
2020-07-28 13:41:01 +0200

    Creates 1 neurons, with 4 inputs

--- nnfs.py
+++ nnfs.py
@@ -1,12 +1,13 @@
 '''
 From Sentdex Youtube channel
-Creates a basic neuron with 3 inputs.
-Associated YT NNFS tutorial: https://www.youtube.com/watch?v=Wo5dMEP_BbI
+Creates 1 neurons, with 4 inputs.
+Associated YT NNFS tutorial: https://www.youtube.com/watch?v=lGLto9Xd7bU
 '''
 
-inputs = [1.2, 5.1, 2.1]
-weights = [3.1, 2.1, 8.7]
+inputs = [1.2, 5.1, 2.1 , 2 ]
+
+weights = [3.1, 2.1, 8.7 , 4]
 bias = 3.0
 
-output = inputs[0]*weights[0] + inputs[1]*weights[1] + inputs[2]*weights[2] + bias
+output = inputs[0]*weights[0] + inputs[1]*weights[1] + inputs[2]*weights[2] + + inputs[3]*weights[3]+ bias
 print(output)
