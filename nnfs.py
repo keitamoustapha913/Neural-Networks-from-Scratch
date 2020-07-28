2455271ae169a24f3d6bbca240121f798b7c6b8d
Mouhamed Moustapha KEITA <keitamoustapha913@gmail.com>
2020-07-28 14:25:01 +0200

    Dot product for a layer of neurons

--- nnfs.py
+++ nnfs.py
@@ -6,20 +6,16 @@ Associated YT NNFS tutorial: https://www.youtube.com/watch?v=lGLto9Xd7bU
 import numpy as np 
 
 
-
-
-inputs = [1.2, 5.1, 2.1 , 2 ]
+inputs = [1, 2 , 3, 2.5 ]
 
 weights = [ [0.2, 0.8, -0.5, 1.0],
             [0.5, -0.91, 0.26, -0.5] ,
             [-0.26, -0.27, 0.17, 0.87] ]
 
-w1 = [0.2, 0.8, -0.5, 1.0]
-bs = [ 2 ]
 biases = [ 2.0 , 3.0, 0.5 ]
 
 
-# Using Dot product approach for one dimensional array and numpy
-output = np.dot(w1 , inputs) + bs
+# Using Dot product approach for a layer
+output = np.dot(weights , inputs) + biases
 
 print( output )
